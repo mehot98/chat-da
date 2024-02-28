6ed6a8004d728dd463e7ad1dcbf8e434f3fbf571
나해란 <nh2545@naver.com>
2024-02-28 14:47:11 +0900

    init: chrome extension 개발을 위한 초기세팅

---
+++ web/manifest.js
@@ -0,0 +1,62 @@
+import fs from "node:fs";
+const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+
+/**
+ * After changing, please reload the extension at `chrome://extensions`
+ * @type {chrome.runtime.ManifestV3}
+ */
+const manifest = {
+  manifest_version: 3,
+  /**
+   * if you want to support multiple languages, you can use the following reference
+   * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Internationalization
+   */
+  name: "ChatDA",
+  version: packageJson.version,
+  description: "삼성의 가전제품에 대해 궁금한 점이 있으신가요? ChatDA가 설명해드리겠습니다!",
+  permissions: ["storage", "sidePanel"],
+  side_panel: {
+    default_path: "src/pages/sidepanel/index.html",
+  },
+  background: {
+    service_worker: "src/pages/background/index.js",
+    type: "module",
+  },
+  action: {
+    default_popup: "src/pages/popup/index.html",
+    default_icon: "ChatDA_icon_32.png",
+  },
+  icons: {
+    16: "ChatDA_icon_16.png",
+    32: "ChatDA_icon_32.png",
+    48: "ChatDA_icon_48.png",
+    128: "ChatDA_icon_128.png",
+  },
+  content_scripts: [
+    {
+      matches: ["http://*/*", "https://*/*", "<all_urls>"],
+      js: ["src/pages/contentInjected/index.js"],
+      // KEY for cache invalidation
+      css: ["assets/css/contentStyle<KEY>.chunk.css"],
+    },
+    {
+      matches: ["http://*/*", "https://*/*", "<all_urls>"],
+      js: ["src/pages/contentUI/index.js"],
+    },
+  ],
+  web_accessible_resources: [
+    {
+      resources: [
+        "assets/js/*.js",
+        "assets/css/*.css",
+        "ChatDA_icon_128.png",
+        "ChatDA_icon_48.png",
+        "ChatDA_icon_32.png",
+        "ChatDA_icon_16.png",
+      ],
+      matches: ["*://*/*"],
+    },
+  ],
+};
+
+export default manifest;
---
+++ web/utils/plugins/custom-dynamic-import.ts
@@ -0,0 +1,24 @@
+import type { PluginOption } from "vite";
+
+export default function customDynamicImport(): PluginOption {
+  return {
+    name: "custom-dynamic-import",
+    renderDynamicImport({ moduleId }) {
+      if (!moduleId.includes("node_modules")) {
+        return {
+          left: `
+          {
+            const dynamicImport = (path) => import(path);
+            if (browser) {
+              dynamicImport(browser.runtime.getURL('./') + 
+            `,
+          right: ".split('../').join(''))};}",
+        };
+      }
+      return {
+        left: "import(",
+        right: ")",
+      };
+    },
+  };
+}
