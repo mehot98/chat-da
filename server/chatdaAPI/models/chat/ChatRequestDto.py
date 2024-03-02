1f40d549b0f7cf7cf1b281bc891cdef29f52f0b1
서다찬 <chn7894@naver.com>
2024-03-02 22:56:39 +0900

    feat: Dto feedback request dto 추가

--- server/chatdaAPI/models/chat/ChatRequestDto.py
+++ server/chatdaAPI/models/chat/ChatRequestDto.py
@@ -1,7 +1,15 @@
+from typing import Literal
+
 from pydantic import BaseModel
 
 
 class ChatRequestDto(BaseModel):
     uuid: str
     content: str
+
+
+class FeedbackRequestDto(BaseModel):
+    uuid: str
+    created_at: str
+    feedback: Literal["긍정", "부정"]
 
