14e9515625c3004f08fe2cd7f33c99c1b8f4a4b5
sj <sj@DESKTOP-6F4NEMJ>
2024-03-04 00:39:20 +0900

    feat: Add prompot
    프롬프트 작성중

--- RAG/prompt.py
+++ RAG/prompt.py
@@ -1,4 +1,7 @@
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import FewShotPromptTemplate, PromptTemplate
+from langchain_core.example_selectors import SemanticSimilarityExampleSelector
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 
 # 답변 생성용 프롬프트
 answer_prompt = PromptTemplate.from_template(
@@ -8,6 +11,72 @@ Question: {question}
 SQL Query: {query}
 SQL Result: {result}
 Answer: """
+)
+
+# SQL 생성용 프롬프트 초반
+# from langchain.chains.sql_database.prompt import SQL_PROMPTS
+mysql_prompt_prefix = """You are a MySQL expert. Given an input question, first create a syntactically correct MySQL query to run, then look at the results of the query and return the answer to the input question.
+Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per MySQL. You can order the results to return the most informative data in the database.
+Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in backticks (`) to denote them as delimited identifiers.
+Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+Pay attention to use CURDATE() function to get the current date, if the question involves "today".
+
+Only use the following tables:
+{table_info}
+
+Use the following format:
+
+User input: User input here
+SQL query: SQL Query to run
+
+Below are a number of examples of questions and their corresponding SQL queries."""
+
+# SQL 예시 프롬프트
+example_prompt = PromptTemplate.from_template(
+    """User input: {input}
+SQL query: {query}"""
+)
+
+# SQL 예시들
+examples = [
+    {
+        "input": "List all artists.",
+        "query": "SELECT * FROM Artist;"
+    },
+    {
+        "input": "Find all albums for the artist 'AC/DC'.",
+        "query": "SELECT * FROM Album WHERE ArtistId = (SELECT ArtistId FROM Artist WHERE Name = 'AC/DC');",
+    },
+    {
+        "input": "List all tracks in the 'Rock' genre.",
+        "query": "SELECT * FROM Track WHERE GenreId = (SELECT GenreId FROM Genre WHERE Name = 'Rock');",
+    },
+    {
+        "input": "Find the total duration of all tracks.",
+        "query": "SELECT SUM(Milliseconds) FROM Track;",
+    }
+]
+
+# SQL 생성용 프롬프트 후반
+mysql_prompt_suffix = """User input: {input}
+SQL query: """
+
+# 사용자의 입력과 관련 있는 예시만 가져오기 위한 selector
+example_selector = SemanticSimilarityExampleSelector.from_examples(
+    examples,
+    OpenAIEmbeddings(),
+    FAISS,
+    k=5,
+    input_keys=["input"],
+)
+
+# SQL 생성용 프롬프트 최종
+sql_prompt = FewShotPromptTemplate(
+    example_selector=example_selector,
+    example_prompt=example_prompt,
+    prefix=mysql_prompt_prefix,
+    suffix=mysql_prompt_suffix,
+    input_variables=["input", "top_k", "table_info"],
 )
 
 
