5c54225b9132759e202ab8fe7da18b248205380b
나해란 <nh2545@naver.com>
2024-03-07 11:32:12 +0900

    design: scroll bar styling

--- web/src/components/ChatbotMain/style.tsx
+++ web/src/components/ChatbotMain/style.tsx
@@ -9,10 +9,39 @@ export const ChatMainWrapper = styled.div`
 `;
 
 export const ChatMessageWrapper = styled.div`
-  width: 100%;
+  width: 98%;
   height: 450px;
   padding: 0 15px;
+  margin: auto;
   overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    background-color: transparent;
+    width: 10px;
+    margin: 1px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: ${theme.bordercolor};
+
+    background-clip: padding-box;
+    border: 1px solid transparent;
+    border-radius: 10px;
+  }
+
+  // ::-webkit-scrollbar-button:vertical:decrement {
+  //   border-bottom: 5px solid ${theme.bordercolor};
+  //   border-top: 5px solid transparent;
+  //   border-left: 5px solid transparent;
+  //   border-right: 5px solid transparent;
+  // }
+
+  // ::-webkit-scrollbar-button:vertical:increment {
+  //   border-bottom: 5px solid transparent;
+  //   border-top: 5px solid ${theme.bordercolor};
+  //   border-left: 5px solid transparent;
+  //   border-right: 5px solid transparent;
+  // }
 `;
 
 export const ChatInputWrapper = styled.div`
