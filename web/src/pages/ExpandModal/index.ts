1990a9cd16a0cb85d7879b8babd4cddf7dea2822
나해란 <nh2545@naver.com>
2024-03-20 17:31:40 +0900

    feat: serach page 생성

--- web/src/pages/ExpandModal/index.ts
+++ web/src/pages/ExpandModal/index.ts
@@ -1,5 +1,6 @@
 import PopularItemPage from "./PopularItemPage";
 import DetailSpecPage from "./DetailSpecPage";
 import CompareSpecPage from "./CompareSpecPage";
+import SearchPage from "./SearchPage";
 
-export { PopularItemPage, DetailSpecPage, CompareSpecPage };
+export { PopularItemPage, DetailSpecPage, CompareSpecPage, SearchPage };
