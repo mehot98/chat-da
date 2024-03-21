00793a618f60329866d7ca99d1d6652c913c91da
나해란 <nh2545@naver.com>
2024-03-21 16:57:13 +0900

    feat: size icon

--- web/src/components/SpecDetailColumn/Subs/SpecSummarySizeIcon/index.tsx
+++ web/src/components/SpecDetailColumn/Subs/SpecSummarySizeIcon/index.tsx
@@ -5,7 +5,15 @@ import specIconPath from "@assets/img/SpecIcon";
 export default function SpecSummarySizeIcon({ sizeSpec }: { sizeSpec: T.SummarySpecType }) {
   return (
     <S.SpecSummaryIconWrapper>
-      <S.SpecSummaryIconImg src={specIconPath.sizePath} alt="spec-icon" />
+      {sizeSpec && sizeSpec.가로 && sizeSpec.가로 !== "없음" && (
+        <S.SpecSummaryIconImgWrapper>
+          <S.widthSpan>{sizeSpec.가로}</S.widthSpan>
+          <S.heightSpan>{sizeSpec.높이}</S.heightSpan>
+          <S.depthSpan>{sizeSpec.깊이}</S.depthSpan>
+          <S.SpecSummaryIconImg src={specIconPath.sizePath} alt="spec-icon" />
+        </S.SpecSummaryIconImgWrapper>
+      )}
+
       {sizeSpec && sizeSpec.제품_타입 && sizeSpec.제품_타입 !== "없음" && (
         <>
           <S.SpecSummaryIconTitle>제품 타입</S.SpecSummaryIconTitle>
--- web/src/components/SpecDetailColumn/Subs/SpecSummarySizeIcon/style.tsx
+++ web/src/components/SpecDetailColumn/Subs/SpecSummarySizeIcon/style.tsx
@@ -8,6 +8,9 @@ export const SpecSummaryIconWrapper = styled.div`
 `;
 
 export const SpecSummaryIconImg = styled.img`
+  position: absolute;
+  right: 14px;
+  bottom: 5px;
   max-width: 145px;
   max-height: 140px;
 `;
@@ -19,4 +22,32 @@ export const SpecSummaryIconTitle = styled.span`
 
 export const SpecSummaryIconDescription = styled.span`
   font-size: 13px;
+`;
+
+export const SpecSummaryIconImgWrapper = styled.div`
+  position: relative;
+  width: 133px;
+  height: 152px;
+`;
+
+const sizeSpan = styled.span`
+  position: absolute;
+  font-size: 12px;
+`;
+
+export const widthSpan = styled(sizeSpan)`
+  top: 0px;
+  left: 56px;
+`;
+
+export const heightSpan = styled(sizeSpan)`
+  color: #3a6da9;
+  top: 77px;
+  left: 1px;
+`;
+
+export const depthSpan = styled(sizeSpan)`
+  color: #3a6da9;
+  top: 5px;
+  left: 107px;
 `;
