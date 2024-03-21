542068995df3ece67ce1b6c7d17adf7dc7c164ac
이승준 <tmdwns7809@gmail.com>
2024-03-21 13:48:11 +0900

    feat: DB 떰프 파일 수정
    할인정보에 중복되는 제품코드가 존재하여 삭제

--- server/chatdaAPI/RAG/make_output.py
+++ server/chatdaAPI/RAG/make_output.py
@@ -135,8 +135,8 @@ def get_output(user_input, search):
     }
 
 
-# 테스트용
-if __name__ == '__main__':
-    res = get_output(user_input='요새 잘 나가는 냉장고가 뭐야?', search=False)
-    print(f"type : {res['type']}")
-    print(f"content : {res['content']}")
+# # 테스트용
+# if __name__ == '__main__':
+#     res = get_output(user_input='요새 잘 나가는 냉장고가 뭐야?', search=False)
+#     print(f"type : {res['type']}")
+#     print(f"content : {res['content']}")
