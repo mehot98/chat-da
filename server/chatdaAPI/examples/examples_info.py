0255c8282591daf6db796b40f754bbddb2ba6e7d
이승준 <tmdwns7809@gmail.com>
2024-03-22 14:33:30 +0900

    feat : 예시 추가

--- server/chatdaAPI/examples/examples_info.py
+++ server/chatdaAPI/examples/examples_info.py
@@ -4,7 +4,10 @@ from chatdaAPI.examples.base_query import join
 examples = [
     {
         "input": "RF85C90D1AP에 대해서 설명해줘",
-        "query": ("SELECT * FROM '냉장고' WHERE `제품_코드`='RF85C90D1AP';\n\n"
+        "query": ("SELECT * FROM '냉장고'"
+                  " JOIN `리뷰_정보` ON `냉장고`.`제품_코드` = `리뷰_정보`.`제품_코드`"
+                  " JOIN `제품_정보` ON `냉장고`.`제품_코드` = `제품_정보`.`제품_코드`"
+                  " WHERE '냉장고'.`제품_코드`='RF85C90D1AP';\n\n"
                   f"{join} WHERE '냉장고'.`제품_코드`='RF85C90D1AP';"),
         "type": INFO,
         "index": 0
@@ -15,5 +18,25 @@ examples = [
                   f"{join} WHERE '냉장고'.`제품명` LIKE '%BESPOKE 냉장고 4도어%';"),
         "type": INFO,
         "index": 1
+    },
+    {
+        "input": "RF85C90D1AP 어때?",
+        "query": ("SELECT * FROM '냉장고'"
+                  " JOIN `리뷰_정보` ON `냉장고`.`제품_코드` = `리뷰_정보`.`제품_코드`"
+                  " JOIN `제품_정보` ON `냉장고`.`제품_코드` = `제품_정보`.`제품_코드`"
+                  " WHERE '냉장고'.`제품_코드`='RF85C90D1AP';\n\n"
+                  f"{join} WHERE '냉장고'.`제품_코드`='RF85C90D1AP';"),
+        "type": INFO,
+        "index": 2
+    },
+    {
+        "input": "RF85C90D1AP 제품 정보",
+        "query": ("SELECT * FROM '냉장고'"
+                  " JOIN `리뷰_정보` ON `냉장고`.`제품_코드` = `리뷰_정보`.`제품_코드`"
+                  " JOIN `제품_정보` ON `냉장고`.`제품_코드` = `제품_정보`.`제품_코드`"
+                  " WHERE '냉장고'.`제품_코드`='RF85C90D1AP';\n\n"
+                  f"{join} WHERE '냉장고'.`제품_코드`='RF85C90D1AP';"),
+        "type": INFO,
+        "index": 3
     },
 ]
--- server/chatdaAPI/examples/examples_recommend.py
+++ server/chatdaAPI/examples/examples_recommend.py
@@ -23,4 +23,19 @@ examples = [
         "type": RECOMMEND,
         "index": 2
     },
+    {
+        "input": "에너지 효율이 좋으면서 리뷰 평점도 높은 냉장고 추천해줘",
+        "query": ("SELECT * FROM '냉장고' JOIN `리뷰_정보` ON `냉장고`.제품_코드 = `리뷰_정보`.제품_코드 "
+                  "WHERE `냉장고`.소비효율등급 <= '1등급' ORDER BY `리뷰_정보`.평점 DESC, `냉장고`.소비효율등급;\n\n"
+                  f"{join} WHERE `냉장고`.소비효율등급 <= '1등급' ORDER BY `리뷰_정보`.평점 DESC, `냉장고`.소비효율등급;"),
+        "type": RECOMMEND,
+        "index": 3
+    },
+    {
+        "input": "냉장실 용량이 크면서 소비 전력이 낮은 냉장고 추천해줘",
+        "query": ("SELECT * FROM '냉장고' ORDER BY `냉장실_용량` DESC, `소비_전력` LIMIT 1;\n\n"
+                  f"{join} ORDER BY `냉장실_용량` DESC, `소비_전력` LIMIT 1;"),
+        "type": RECOMMEND,
+        "index": 4
+    },
 ]
