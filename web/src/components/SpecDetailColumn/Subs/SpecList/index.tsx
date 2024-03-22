a9538d9b904204e72046b3ab5f9a619c0afb01ad
나해란 <nh2545@naver.com>
2024-03-22 15:32:21 +0900

    design: 스펙 gap 수정

--- web/src/components/SpecDetailColumn/Subs/SpecList/index.tsx
+++ web/src/components/SpecDetailColumn/Subs/SpecList/index.tsx
@@ -5,10 +5,10 @@ export default function SpecList({ rawSpec }: { rawSpec: any }) {
   return (
     <S.SpecListWrapper>
       {Object.keys(rawSpec).map((key) => (
-        <>
+        <S.SpecLineWrapper key={key}>
           <S.SpecTitle>{key}</S.SpecTitle>
           <S.SpecDetail>{rawSpec[key]}</S.SpecDetail>
-        </>
+        </S.SpecLineWrapper>
       ))}
     </S.SpecListWrapper>
   );
