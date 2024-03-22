ed7f2bc6c80e0237566c9219ebd56ad2fd1305a8
서다찬 <chn7894@naver.com>
2024-03-22 16:13:48 +0900

    fix: db table 이름 수정

--- server/chatdaAPI/examples/examples_info.py
+++ server/chatdaAPI/examples/examples_info.py
@@ -4,15 +4,15 @@ from chatdaAPI.examples.base_query import join
 examples = [
     {
         "input": "RF85C90D1AP에 대해서 설명해줘",
-        "query": ("SELECT * FROM '냉장고' WHERE `제품_코드`='RF85C90D1AP';\n\n"
-                  f"{join} WHERE '냉장고'.`제품_코드`='RF85C90D1AP';"),
+        "query": ("SELECT * FROM 'refridgerator' WHERE `제품_코드`='RF85C90D1AP';\n\n"
+                  f"{join} WHERE 'refridgerator'.`제품_코드`='RF85C90D1AP';"),
         "type": INFO,
         "index": 0
     },
     {
         "input": "BESPOKE 냉장고 4도어 제품에 대해 설명해줘",
-        "query": ("SELECT * FROM '냉장고' WHERE `제품명` LIKE '%BESPOKE 냉장고 4도어%';\n\n"
-                  f"{join} WHERE '냉장고'.`제품명` LIKE '%BESPOKE 냉장고 4도어%';"),
+        "query": ("SELECT * FROM 'refridgerator' WHERE `제품명` LIKE '%BESPOKE 냉장고 4도어%';\n\n"
+                  f"{join} WHERE 'refridgerator'.`제품명` LIKE '%BESPOKE 냉장고 4도어%';"),
         "type": INFO,
         "index": 1
     },
--- server/chatdaAPI/examples/examples_recommend.py
+++ server/chatdaAPI/examples/examples_recommend.py
@@ -4,22 +4,22 @@ from chatdaAPI.examples.base_query import join
 examples = [
     {
         "input": "150만원 이내의 큰 냉장고를 찾고 있는데 추천해 줄 수 있어?",
-        "query": ("SELECT * FROM '냉장고' WHERE `가격`<='1500000 원' ORDER BY `전체_용량` DESC;\n\n"
-                  f"{join} WHERE `냉장고`.`가격`<='1500000 원' ORDER BY `냉장고`.`전체_용량` DESC;"),
+        "query": ("SELECT * FROM 'refridgerator' WHERE `가격`<='1500000 원' ORDER BY `전체_용량` DESC;\n\n"
+                  f"{join} WHERE `refridgerator`.`가격`<='1500000 원' ORDER BY `refridgerator`.`전체_용량` DESC;"),
         "type": RECOMMEND,
         "index": 0
     },
     {
         "input": "냉장실 용량대비 가장 가격이 저렴한 냉장고를 추천해줘",
-        "query": ("SELECT * FROM '냉장고' ORDER BY `냉장실_용량` DESC, `가격` DESC;\n\n"
-                  f"{join} ORDER BY `냉장고`.`전체_용량` DESC, `냉장고`.`가격` DESC;"),
+        "query": ("SELECT * FROM 'refridgerator' ORDER BY `냉장실_용량` DESC, `가격` DESC;\n\n"
+                  f"{join} ORDER BY `refridgerator`.`전체_용량` DESC, `refridgerator`.`가격` DESC;"),
         "type": RECOMMEND,
         "index": 1
     },
     {
         "input": "냉장고 중에 100만원대의 냉장고를 10개 추천해줘",
-        "query": ("SELECT * FROM '냉장고' WHERE `가격`<='1500000 원' ORDER BY `전체_용량` DESC;\n\n"
-                  f"{join} WHERE `냉장고`.`가격`<='1000000 원' ORDER BY `냉장고`.`전체_용량` DESC LIMIT 10;"),
+        "query": ("SELECT * FROM 'refridgerator' WHERE `가격`<='1500000 원' ORDER BY `전체_용량` DESC;\n\n"
+                  f"{join} WHERE `refridgerator`.`가격`<='1000000 원' ORDER BY `refridgerator`.`전체_용량` DESC LIMIT 10;"),
         "type": RECOMMEND,
         "index": 2
     },
