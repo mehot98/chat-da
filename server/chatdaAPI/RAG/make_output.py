1733707e1b1964e835357f6456bcc62cc2fa440d
이승준 <tmdwns7809@gmail.com>
2024-03-27 17:55:44 +0900

    feat: 테스트 진행중

--- server/chatdaAPI/RAG/make_output.py
+++ server/chatdaAPI/RAG/make_output.py
@@ -152,11 +152,11 @@ def get_output(user_input, search):
     }
 
 
-# # 체인 중간 과정 보기
-# set_debug(True)
-#
-# # 테스트용
-# if __name__ == '__main__':
-#     res = get_output(user_input='요새 잘 나가는 냉장고가 뭐야?', search=False)
-#     print(f"type : {res['type']}")
-#     print(f"content : {res['content']}")
+# 체인 중간 과정 보기
+set_debug(True)
+
+# 테스트용
+if __name__ == '__main__':
+    res = get_output(user_input='크기가 큰 냉장고 순위 알려줘', search=True)
+    print(f"type : {res['type']}")
+    print(f"content : {res['content']}")
