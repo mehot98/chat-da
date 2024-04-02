3b6bcf1cdafce00b68e22ad619b924f7089fb485
서다찬 <chn7894@naver.com>
2024-04-02 20:44:38 +0900

    Update examples_info.py

--- server/chatdaAPI/examples/examples_info.py
+++ server/chatdaAPI/examples/examples_info.py
@@ -5,8 +5,6 @@ examples = [
     {
         "input": "RF85C90D1AP에 대해서 설명해줘",
         "query": ("SELECT * FROM `refridgerators`"
-                  " JOIN `refridgerator_reviews` ON `refridgerators`.`제품_코드` = `refridgerator_reviews`.`제품_코드`"
-                  " JOIN `refridgerator_details` ON `refridgerators`.`제품_코드` = `refridgerator_details`.`제품_코드`"
                   " WHERE `가격` is not null AND `refridgerators`.`제품_코드`='RF85C90D1AP';\n\n"
                   f"{join} WHERE `가격` is not null AND `refridgerators`.`제품_코드`='RF85C90D1AP';"),
         "type": INFO,
@@ -22,8 +20,6 @@ examples = [
     {
         "input": "RF85C90D1AP 어때?",
         "query": ("SELECT * FROM `refridgerators`"
-                  " JOIN `refridgerator_reviews` ON `refridgerators`.`제품_코드` = `refridgerator_reviews`.`제품_코드`"
-                  " JOIN `refridgerator_details` ON `refridgerators`.`제품_코드` = `refridgerator_details`.`제품_코드`"
                   " WHERE `가격` is not null AND `refridgerators`.`제품_코드`='RF85C90D1AP';\n\n"
                   f"{join} WHERE `가격` is not null AND `refridgerators`.`제품_코드`='RF85C90D1AP';"),
         "type": INFO,
@@ -32,8 +28,6 @@ examples = [
     {
         "input": "RF85C90D1AP 제품 정보",
         "query": ("SELECT * FROM `refridgerators`"
-                  " JOIN `refridgerator_reviews` ON `refridgerators`.`제품_코드` = `refridgerator_reviews`.`제품_코드`"
-                  " JOIN `refridgerator_details` ON `refridgerators`.`제품_코드` = `refridgerator_details`.`제품_코드`"
                   " WHERE `가격` is not null AND `refridgerators`.`제품_코드`='RF85C90D1AP';\n\n"
                   f"{join} WHERE `가격` is not null AND `refridgerators`.`제품_코드`='RF85C90D1AP';"),
         "type": INFO,
